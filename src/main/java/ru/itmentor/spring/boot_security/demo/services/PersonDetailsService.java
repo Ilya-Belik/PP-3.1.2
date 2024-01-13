0aaf75bee4c8a9d25cfc5b913c5c0323023709c2
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2024-01-13 22:10:06 +0500

    PersonServiceImpl.java

--- src/main/java/ru/itmentor/spring/boot_security/demo/services/PersonDetailsService.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/services/PersonDetailsService.java
@@ -1,28 +1,89 @@
 package ru.itmentor.spring.boot_security.demo.services;
+
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.itmentor.spring.boot_security.demo.models.Person;
-import ru.itmentor.spring.boot_security.demo.repositories.PeopleRepository;
-import ru.itmentor.spring.boot_security.demo.security.PersonDetails;
+import ru.itmentor.spring.boot_security.demo.models.Role;
+import ru.itmentor.spring.boot_security.demo.repositories.PersonRepository;
+import ru.itmentor.spring.boot_security.demo.repositories.RoleRepository;
+import ru.itmentor.spring.boot_security.demo.util.PersonNotFoundException;
+
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
-public class PersonDetailsService implements UserDetailsService {
+public class PersonServiceImpl implements PersonService {
 
-    private final PeopleRepository peopleRepository;
+    private final RoleRepository roleRepository;
+    private final PersonRepository personRepository;
+
     @Autowired
-    public PersonDetailsService(PeopleRepository peopleRepository) {
-        this.peopleRepository = peopleRepository;
+    public PersonServiceImpl(RoleRepository roleRepository, PersonRepository personRepository) {
+        this.roleRepository = roleRepository;
+        this.personRepository = personRepository;
+    }
+    @Override
+    public Optional<Person> findByUsername(String username) {
+        return personRepository.findByUsername(username);
     }
 
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Person> person = peopleRepository.findByUsername(username);
-        if(person.isEmpty())
-            throw new UsernameNotFoundException("Такого пользователя нет");
-        return new PersonDetails(person.get());
+        Optional<Person> personOptional = personRepository.findByUsername(username);
+        if (!personOptional.isPresent()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        Person person = personOptional.get();
+        Hibernate.initialize(person.getRole());
+        return new User(person.getUsername(), person.getPassword(), person.getRole());
+    }
+
+    @Transactional
+    @Override
+    public void addNewPerson(Person person) {
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleRepository.findByName("ROLE_USER"));
+        person.setRole(roles);
+        personRepository.save(person);
+    }
+
+    @Transactional
+    @Override
+    public List<Person> getAll() {
+        return personRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public Person findById(int id) {
+        return personRepository.findById(id)
+                .orElseThrow(PersonNotFoundException::new);
+    }
+
+    @Transactional
+    @Override
+    public void delete(int id) {
+        personRepository.deleteById(id);
+    }
+
+    @Transactional
+    @Override
+    public Person update(Person updatedPerson, int id) {
+        Person personForUpdating = personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException());
+        personForUpdating.setName(updatedPerson.getName());
+        personForUpdating.setUsername(updatedPerson.getUsername());
+        personForUpdating.setPassword(updatedPerson.getPassword());
+        personRepository.save(personForUpdating);
+
+        return personForUpdating;
     }
 }
