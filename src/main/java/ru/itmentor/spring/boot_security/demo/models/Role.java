8a71ec42361edaf0e1cbb0e50ffd11cccc5ce28e
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2024-01-14 17:10:26 +0500

    Update Role.java

--- src/main/java/ru/itmentor/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/models/Role.java
@@ -1,24 +1,27 @@
 package ru.itmentor.spring.boot_security.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@JsonIgnoreProperties({"person"})
 @Entity
 @Table(name="roles")
 public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
 
     @Column(name = "name_role")
     private String name;
-
-    @ManyToMany(mappedBy = "role")
-    private Set<Person> persons;
 
     public Role() {
     }
@@ -44,16 +47,21 @@ public class Role implements GrantedAuthority {
         this.name = name;
     }
 
-    public Set<Person> getPersons() {
-        return persons;
+    public Person getPerson() {
+        return person;
     }
 
-    public void setPersons(Set<Person> persons) {
-        this.persons = persons;
+    public void setPerson(Person person) {
+        this.person = person;
     }
 
     @Override
     public String getAuthority() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return name.toString();
     }
 }
